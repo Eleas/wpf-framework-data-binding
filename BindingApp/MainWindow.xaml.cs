590fc2f5b1bf1669e0cc72343d19e5f5e6657169
Björn Paulsen <bjorn.paulsen@gmail.com>
2023-10-15 13:53:21 +0200

    upload app files
    We probably need some kind of gitignore file later on, but hey

---
+++ BindingApp/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace BindingApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TagControl.TagControl TagControl;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TagControl = new TagControl.TagControl(this);
+
+            TagControl.Show();
+        }
+    }
+}
---
+++ BindingApp/TagControl/TagItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Media;
+
+namespace BindingApp.TagControl
+{
+    public class TagItem
+    {
+        public TagItem(string name, SolidColorBrush color, bool isSelected = false)
+        {
+            Name = name;
+            Color = color;
+            IsSelected = isSelected;
+        }
+
+        public string Name { get; set; }
+        public SolidColorBrush Color { get; set; }
+        public bool IsSelected { get; set; } = false;
+    }
+}
