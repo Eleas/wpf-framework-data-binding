a87a94eeea63d1adc60f23b359fa0931c0b258e6
Björn Paulsen <bjorn.paulsen@decerno.se>
2023-10-15 22:02:59 +0200

    feature/add a ViewModel to TagControl.xaml #2 done

--- BindingApp/TagControl/TagControl.xaml.cs
+++ BindingApp/TagControl/TagControl.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.ObjectModel;
-using System.Windows;
-using System.Windows.Media;
+﻿using System.Windows;
 
 namespace BindingApp.TagControl
 {
@@ -9,34 +7,12 @@ namespace BindingApp.TagControl
     /// </summary>
     public partial class TagControl : Window
     {
-        private ObservableCollection<TagItem> _items;
-
-        // Build the list of tag items.
-        private ObservableCollection<TagItem> CreateTagItemList()
-        {
-            var items = new ObservableCollection<TagItem>
-            {
-                new TagItem("Cool tag", new SolidColorBrush(Colors.DarkBlue)),
-                new TagItem("Strange tag", new SolidColorBrush(Colors.Crimson)),
-                new TagItem("Lepton", new SolidColorBrush(Colors.Chartreuse))
-            };
-            return items;
-        }
 
         public TagControl(Window parentWindow)
         {
             parentWindow.Closed += (s, e) => Close();
             Topmost = true;
             InitializeComponent();
-
-            // Make empty collection exist.
-            Items = CreateTagItemList();
-
-            // Add the data for the item control.
-            lvDataBinding.ItemsSource = Items;
         }
-
-        // Expose datamodel to XAML.
-        public ObservableCollection<TagItem> Items { get => _items; set => _items = value; }
     }
 }
---
+++ BindingApp/TagControl/TagControlViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Media;
+
+namespace BindingApp.TagControl
+{
+    public class TagControlViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<TagItem> _items;
+
+        public ObservableCollection<TagItem> Items
+        {
+            get { return _items; }
+            set
+            {
+                _items = value;
+                OnPropertyChanged(nameof(TagItem));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        // Build the list of tag items.
+        private ObservableCollection<TagItem> CreateTagItemList()
+        {
+            var items = new ObservableCollection<TagItem>
+            {
+                new TagItem("Cool tag", new SolidColorBrush(Colors.DarkBlue)),
+                new TagItem("Strange tag", new SolidColorBrush(Colors.Crimson)),
+                new TagItem("Lepton", new SolidColorBrush(Colors.Chartreuse))
+            };
+            return items;
+        }
+
+        public TagControlViewModel()
+        {
+            // Make empty collection exist.
+            Items = CreateTagItemList();
+        }
+    }
+}
